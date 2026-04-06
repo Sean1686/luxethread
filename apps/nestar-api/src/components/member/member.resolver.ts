4f66fc5832ff053837db515424715b244efe70cb
Sean1686 <khudayberdi11@gmail.com>
2026-04-07 01:58:18 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,32 +1,38 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
+import { LoginInput, MemberInput } from '../../libs/DTO/member/member.input';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
 
 @Resolver()
 export class MemberResolver {
     constructor(private readonly memberService: MemberService) {}
 
     @Mutation(() => String)
-    public async signUp(): Promise<string> {
-        console.log("Mutation signUp called");
-        return this.memberService.signUp();
+    @UsePipes(ValidationPipe)
+    public async signup(@Args("input") input: MemberInput): Promise<string> {
+        console.log("Mutation signup called");
+        console.log("Input received:", input);
+        return this.memberService.signup();
     }
 
     @Mutation(() => String)
-    public async login(): Promise<string> {
+    @UsePipes(ValidationPipe)
+    public async login(@Args("input") input: LoginInput): Promise<string> {
         console.log("Mutation login called");
+        console.log("Input received:", input);
         return this.memberService.login();
     }
 
     @Mutation(() => String)
-    public async updateMember(): Promise<string> {
-        console.log("Mutation updateMember called");
-        return this.memberService.updateMember();
+    public async updatemember(@Args("input") input: MemberInput): Promise<string> {
+        console.log("Mutation updatemember called");
+        return this.memberService.updatemember();
     }
 
     
     @Query(() => String)
-    public async getMember(): Promise<string> {
-        console.log("Query getMember called");
-        return this.memberService.getMember();
+    public async getmember(): Promise<string> {
+        console.log("Query getmember called");
+        return this.memberService.getmember();
     }
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { MemberInput } from '../../libs/DTO/member/member.input';
 
 @Injectable()
 export class MemberService {
 
     constructor(@InjectModel("Member") private readonly memberModel: Model<null>) {}
 
-    public async signUp(): Promise<string> {
-          return "signUp executed"
+    public async signup(): Promise<string> {
+          return "signup executed"
     }
 
     public async login(): Promise<string> {
           return "login executed"
     }
 
-    public async updateMember(): Promise<string> {
-          return "updateMember executed"
+    public async updatemember(): Promise<string> {
+          return "updatemember executed"
     }
 
-    public async getMember(): Promise<string> {
-          return "getMember executed"
+    public async getmember(): Promise<string> {
+          return "getmember executed"
     }
 
 }
