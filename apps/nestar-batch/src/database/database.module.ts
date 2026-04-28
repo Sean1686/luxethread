12ccc44cd7e7c14a9d1fcba9668aa0c5d14d42ed
Sean1686 <khudayberdi11@gmail.com>
2026-04-28 18:54:27 +0900

    fix: modify batch module and connect to MongoDB

---
+++ apps/nestar-batch/src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+@Module({
+	imports: [
+		MongooseModule.forRootAsync({
+			useFactory: () => ({
+				uri:
+					process.env.NODE_ENV === 'production' ? process.env.MONGO_PRODUCTION_URI : process.env.MONGO_DEVELOPMENT_URI,
+			}),
+		}),
+	],
+	exports: [MongooseModule],
+})
+export class DatabaseModule {
+	constructor(@InjectConnection() private readonly connection: Connection) {
+		if (connection.readyState === 1) {
+			console.log(
+				`MongoDB connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} environment!`,
+			);
+		} else {
+			console.error('Failed to connect to MongoDB!');
+		}
+	}
+}
